5ab7dd771c1eb37a00431f45f6b3124038d65b42
tatsenko-m <tatsenko.m@yandex.ru>
2023-09-30 13:22:19 +0300

    подготовить директорию проекта

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: { main: './src/pages/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.[hash].js',
+        publicPath: ''
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: "images/[name].[hash][ext]"
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: "fonts/[name].[hash][ext]"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin()
+  ]
+};
