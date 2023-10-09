f3ac5bf9dbb732cf2ada01d388a4be93c3bf53ba
tatsenko-m <tatsenko.m@yandex.ru>
2023-10-10 00:21:20 +0300

    feat: оживить аккордеон

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,13 @@ import "./index.css";
 import { initialItems } from "../utils/constants";
 import ItemCard from "../components/ItemCard";
 
+const accordionButtons = document.querySelectorAll(
+  ".cart-items__accordion-btn"
+);
+const addedItemsList = document.querySelector("#added-items");
+const unavailableItemsList = document.querySelector("#unavailable-items");
+const deliverySection = document.querySelector(".delivery");
+
 function renderItems(itemsArr, listSelector, templateSelector, isUnavailable) {
   const itemsList = document.querySelector(listSelector);
   itemsArr.forEach((item) => {
@@ -10,6 +17,21 @@ function renderItems(itemsArr, listSelector, templateSelector, isUnavailable) {
     itemsList.append(itemCardElement);
   });
 }
+
+accordionButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const content = button.parentElement.nextElementSibling;
+
+    if (content.style.display === "none") {
+      content.style.display = "flex";
+    } else {
+      content.style.display = "none";
+      if (button === accordionButtons[0]) {
+        button.parentElement.style.marginBottom = "17px";
+      }
+    }
+  });
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   renderItems(initialItems, "#added-items", "#item-template", false);
