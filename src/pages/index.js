44761b1f699bbeecd3b4ecb228d3df7597387d86
tatsenko-m <tatsenko.m@yandex.ru>
2023-10-10 22:54:35 +0300

    feat: сделать секцию total динамической

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,79 @@ function renderItems(itemsArr, listSelector, templateSelector, isUnavailable) {
   });
 }
 
+function updateCartInfo() {
+  const counterInputs = document.querySelectorAll(
+    ".cart-items__list .counter__input"
+  );
+
+  let totalQuantity = 0;
+  let totalNewCost = 0;
+  let totalOldCost = 0;
+
+  counterInputs.forEach((input) => {
+    const itemCard = input.closest(".item");
+
+    const checkbox = itemCard.querySelector(".checkbox-input");
+    if (checkbox && !checkbox.checked) {
+      return;
+    }
+
+    const quantity = parseInt(input.value) || 0;
+
+    const newCost = parseFloat(
+      itemCard
+        .querySelector(".item__price-new-number")
+        .textContent.replace(/\s+/g, "")
+        .replace(",", ".")
+    );
+    const oldCost = parseFloat(
+      itemCard
+        .querySelector(".item__price-old")
+        .textContent.replace(/\s+/g, "")
+        .replace(",", ".")
+    );
+
+    totalQuantity += quantity;
+    totalNewCost += newCost;
+    totalOldCost += oldCost;
+  });
+
+  const totalQuantityElement = document.getElementById("total-quantity");
+  totalQuantityElement.textContent = formatQuantity(totalQuantity);
+
+  const totalNewCostElement = document.getElementById("total-new-cost");
+  totalNewCostElement.textContent = formatPrice(totalNewCost);
+
+  const totalOldCostElement = document.getElementById("total-old-cost");
+  totalOldCostElement.textContent = formatPrice(totalOldCost);
+
+  const totalDiscountElement = document.getElementById("total-discount");
+  const totalDiscount = totalOldCost - totalNewCost;
+  totalDiscountElement.textContent = formatPrice(totalDiscount);
+}
+
+
+function formatQuantity(quantity) {
+  if (quantity % 10 === 1 && quantity % 100 !== 11) {
+    return `${quantity} товар`;
+  } else if (
+    (quantity % 10 >= 2 && quantity % 10 <= 4) &&
+    !(quantity % 100 >= 12 && quantity % 100 <= 14)
+  ) {
+    return `${quantity} товара`;
+  } else {
+    return `${quantity} товаров`;
+  }
+}
+
+function formatPrice(price) {
+  const formattedPrice = price
+    .toFixed(0)
+    .replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+    .replace(".", ",");
+  return `${formattedPrice} сом`;
+}
+
 accordionButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const content = button.parentElement.nextElementSibling;
@@ -36,4 +109,24 @@ accordionButtons.forEach((button) => {
 document.addEventListener("DOMContentLoaded", () => {
   renderItems(initialItems, "#added-items", "#item-template", false);
   renderItems(initialItems, "#unavailable-items", "#item-template", true);
+
+  const counterInputs = document.querySelectorAll(
+    ".cart-items__list .counter__input"
+  );
+  counterInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      updateCartInfo();
+    });
+  });
+
+  const checkboxes = document.querySelectorAll(
+    ".cart-items__list .checkbox-input"
+  );
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", () => {
+      updateCartInfo();
+    });
+  });
+
+  updateCartInfo();
 });
